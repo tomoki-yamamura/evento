8650ab77ac049258320ebe0fec0c95b40bdc3cbb
tomoki-yamamura <progtab17@gmail.com>
2024-06-30 17:35:18 +0800

    fetch server component

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -6,15 +6,26 @@ type EventsPageProps = {
   }
 }
 
-export default function EventsPage({params}: EventsPageProps) {
+export default async function EventsPage({ params }: EventsPageProps) {
   const city = params.city
-  
+
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+  );
+  const events = await response.json()
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1>
         {city === "all" && "All Events"}
-        {city !== "all" &&  `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
-        </H1>
+        {city !== "all" && `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+
+      {
+        events.map(event => (
+          <section key={event.id}>{event.name}</section>
+        ))
+      }
     </main>
   )
 }
